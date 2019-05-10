3322c46e8dab8e7d3249694118db8d11f53125f1
Karel Petrak <karel.petrak@sourcefabric.org>
2019-05-10 13:25:24 +0200

    Added multiple configurations
    Added:
    Planning to "installed apps"
    default time zone and timezone codes
    languages
    newsml provider
    publish queue expiry 
    schema and validation for multimedia
    template for export as article

--- server/settings.py
+++ server/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'apps.languages',
     'anp.io',
     'anp.photo',
+    'planning'
 ]
 
 RENDITIONS = {
@@ -60,3 +61,126 @@ if env('REDIS_PORT'):
 BROKER_URL = env('CELERY_BROKER_URL', REDIS_URL)
 
 SECRET_KEY = env('SECRET_KEY', '')
+
+DEFAULT_TIMEZONE = "Europe/Amsterdam"
+
+DEFAULT_LANGUAGE = 'nl'
+LANGUAGES = [
+    {'language': 'nl', 'label': 'Dutch', 'source': True, 'destination': True},
+    {'language': 'fr', 'label': 'French', 'source': True, 'destination': True},
+    {'language': 'en', 'label': 'English', 'source': False, 'destination': False},
+    {'language': 'de', 'label': 'German', 'source': False, 'destination': False},
+    {'language': 'ja', 'label': 'Japanese', 'source': False, 'destination': False},
+    {'language': 'es', 'label': 'Spanish', 'source': False, 'destination': False},
+    {'language': 'ru', 'label': 'Russian', 'source': False, 'destination': False}
+]
+
+TIMEZONE_CODE = {
+    'aus': 'America/Chicago',
+    'bat': 'Asia/Manila',
+    'bgl': 'Asia/Kolkata',
+    'cav': 'Asia/Manila',
+    'cat': 'Europe/Rome',
+    'chb': 'Asia/Bangkok',
+    'chd': 'America/Phoenix',
+    'chm': 'America/New_York',
+    'cos': 'America/Denver',
+    'cpn': 'America/Chicago',
+    'cri': 'America/New_York',
+    'dal': 'America/Chicago',
+    'dlf': 'Europe/Amsterdam',
+    'drs': 'Europe/Berlin',
+    'ftc': 'America/Denver',
+    'gdh': 'Asia/Kolkata',
+    'grn': 'Europe/Paris',
+    'hlb': 'America/Los_Angeles',
+    'hrt': 'America/Chicago',
+    'irv': 'America/Los_Angeles',
+    'ist': 'Asia/Istanbul',
+    'kws': 'Asia/Tokyo',
+    'lac': 'Europe/Paris',
+    'lee': 'America/New_York',
+    'mbf': 'America/New_York',
+    'mfn': 'America/Los_Angeles',
+    'nwb': 'Europe/London',
+    'pav': 'Europe/Rome',
+    'rlh': 'America/New_York',
+    'roz': 'Europe/Rome',
+    'shg': 'Asia/Shanghai',
+    'sjc': 'America/Los_Angeles',
+    'ssk': 'Asia/Seoul',
+    'svl': 'America/Los_Angeles',
+    'tai': 'Asia/Taipei',
+    'ups': 'Europe/Vienna',
+    'wst': 'America/Indiana/Indianapolis'
+}
+
+# This value gets injected into NewsML 1.2 and G2 output documents.
+NEWSML_PROVIDER_ID = 'ANP'
+ORGANIZATION_NAME = env('ORGANIZATION_NAME', 'ANP')
+ORGANIZATION_NAME_ABBREVIATION = env('ORGANIZATION_NAME_ABBREVIATION', 'ANP')
+
+PUBLISH_QUEUE_EXPIRY_MINUTES = 60 * 24 * 10  # 10d
+
+# schema for images, video, audio
+SCHEMA = {
+    'picture': {
+        'headline': {'required': False},
+        'description_text': {'required': True},
+        'credit': {'required': False},
+        'byline': {'displayOnMediaEditor': False},
+        'copyrightnotice': {'displayOnMediaEditor': False},
+    },
+    'video': {
+        'headline': {'required': False},
+        'description_text': {'required': True},
+        'media_type': {'required': False},
+        'credit': {'required': False},
+        'byline': {'displayOnMediaEditor': False},
+        'copyrightnotice': {'displayOnMediaEditor': False},
+    },
+}
+
+# editor for images, video, audio
+EDITOR = {
+    'picture': {
+        'headline': {'order': 1, 'sdWidth': 'full'},
+        'description_text': {'order': 2, 'sdWidth': 'full', 'textarea': True},
+        'credit': {'order': 3, 'sdWidth': 'full'},
+        'byline': {'displayOnMediaEditor': False},
+        'copyrightnotice': {'displayOnMediaEditor': False},
+    },
+    'video': {
+        'headline': {'order': 2, 'sdWidth': 'full'},
+        'description_text': {'order': 3, 'sdWidth': 'full', 'textarea': True},
+        'media_type': {'order': 4, 'sdWidth': 'full'},
+        'credit': {'order': 5, 'sdWidth': 'full'},
+        'byline': {'displayOnMediaEditor': False},
+        'copyrightnotice': {'displayOnMediaEditor': False},
+    },
+}
+
+SCHEMA['audio'] = SCHEMA['video']
+EDITOR['audio'] = EDITOR['video']
+
+# Template for "export as article" from planning
+PLANNING_EXPORT_BODY_TEMPLATE = '''
+{% for item in items %}
+{% set pieces = [
+    item.get('planning_date') | format_datetime(date_format='%H:%M'),
+    item.get('slugline'),
+    item.get('name'),
+] %}
+<h2>{{ pieces|select|join(' - ') }}</h2>
+{% if item.coverages %}<p>{{ item.coverages | join(' - ') }}</p>{% endif %}
+{% if item.get('description_text') or item.get('links') %}
+<p>{{ item.description_text }}{% if item.get('links') %} URL: {{ item.links | join(' ') }}{% endif %}</p>
+{% endif %}
+{% if item.contacts %}{% for contact in item.contacts %}
+<p>{{ contact.honorific }} {{ contact.first_name }} {{ contact.last_name }}{% if contact.contact_email %} - {{ contact.contact_email|join(' - ') }}{% endif %}{% if contact.contact_phone %} - {{ contact.contact_phone|selectattr('public')|join(' - ', attribute='number') }}{% endif %}</p>
+{% endfor %}{% endif %}
+{% if item.event and item.event.location %}
+<p>{{ item.event.location|join(', ', attribute='name') }}</p>
+{% endif %}
+{% endfor %}
+'''
