d21526f319b97076a36a4acbf1a1ac78b503d9e8
olegpshenichniy <olegpshenichniy@gmail.com>
2019-11-28 15:32:12 +0100

    Eslint.

--- client/anp/talpa-video/index.ts
+++ client/anp/talpa-video/index.ts
@@ -2,7 +2,6 @@ import angular from 'angular';
 import { reactToAngular1 } from 'superdesk-ui-framework';
 import TalpaVideoSearchPanel from './talpaVideoSearchPanel';
 
-
 export default angular.module('anp.talpaVideo', [])
   .component('talpaVideoSearchPanel', reactToAngular1(TalpaVideoSearchPanel, ['params', 'scope']))
   .run(['$templateCache', ($templateCache) => {
--- client/anp/talpa-video/talpaVideoSearchPanel.tsx
+++ client/anp/talpa-video/talpaVideoSearchPanel.tsx
@@ -1,56 +1,10 @@
 import React from 'react';
-import { gettext } from 'superdesk-core/scripts/core/utils';
-
-
-interface ISearchParams {
-  // searchParam: string;
-}
-
-
-interface IProps {
-  params: ISearchParams;
-}
-
-
-interface IState extends Partial<ISearchParams> {
-}
-
-
-const FIELDS = [
-  // {key: 'searchParam', label: gettext('Search'), type: 'text'},
-];
-
 
 /**
- * This component updates props.params, using PureComponent to avoid re-rendering on such change.
+ * This component will be enhanced with fields when new Talpa API will be used.
  */
-export default class TalpaVideoSearchPanel extends React.PureComponent<IProps, IState> {
-
-  readonly state = Object.assign({}, this.props.params);
-
-  onChange(key: string, val: string) {
-    const update: IState = {
-      [key]: val,
-    };
-
-    this.setState(update, () => {
-      Object.assign(this.props.params, this.state);
-    });
-  }
-
-  render() {
-    return (
-      <fieldset>
-        {FIELDS.map((field) => (
-          <div key={field.key} className="field">
-            <label className="search-label">{field.label}</label>
-            <input type={field.type}
-                   name={field.key} onChange={(e) => this.onChange(field.key, e.target.value)}
-                   value={this.state[field.key]}
-            />
-          </div>
-        ))}
-      </fieldset>
-    );
-  }
+export default class TalpaVideoSearchPanel extends React.PureComponent {
+    render() {
+        return '';
+    }
 }
