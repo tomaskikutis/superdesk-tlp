d59bcbcf39d9167516f93df300f5d87ecd61e6cf
Petr Jasek <jasekpetr@gmail.com>
2020-03-13 10:57:07 +0100

    fix ninjs formatter
    family_id didn't work for duplicates,
    so resolving original via traversing rewrites
    
    SDESK-5085

--- server/tests/custom_ninjs_formatter_test.py
+++ server/tests/custom_ninjs_formatter_test.py
@@ -1,18 +1,36 @@
 
 import unittest
 
+from superdesk.tests import TestCase
 from anp.formatters import CustomNINJSFormatter
 
 
-class CustomNINJSFormatterTestCase(unittest.TestCase):
-
-    def test_family_id(self):
-        item = {'language': 'en', 'family_id': 'foo', 'type': 'text', 'copyrightholder': 'cp'}
-        formatter = CustomNINJSFormatter()
-        ninjs = formatter._transform_to_ninjs(item, {})
-        self.assertEqual(ninjs['family_id'], item['family_id'])
-        self.assertEqual(ninjs['language'], item['language'])
+class MetadataTestCase(unittest.TestCase):
 
     def test_can_format(self):
         formatter = CustomNINJSFormatter()
         self.assertTrue(formatter.can_format('custom_ninjs', {}))
+
+
+class CustomNINJSFormatterTestCase(TestCase):
+
+    def test_family_id(self):
+        items = [
+            {'_id': 'orig'},
+            {'_id': 'next', 'rewrite_of': 'orig'},
+            {'_id': 'last', 'rewrite_of': 'next'},
+        ]
+
+        for item in items:
+            item.update({'language': 'en', 'type': 'text', 'copyrightholder': 'foo'})
+
+        self.app.data.insert('archive', items)
+
+        formatter = CustomNINJSFormatter()
+
+        for i, item in enumerate(items):
+            ninjs = formatter._transform_to_ninjs(items[i], {})
+            if i == 0:
+                self.assertIsNone(ninjs.get('original'))
+            else:
+                self.assertEqual('orig', ninjs['original'])
